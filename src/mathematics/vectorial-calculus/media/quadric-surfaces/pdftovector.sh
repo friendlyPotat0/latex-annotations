de27854cedd136b2b08fa0558adee0fa7e613f6c
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-07-18 07:39:10 -0500

    Typo fixed, resource added

--- src/mathematics/vectorial-calculus/media/quadric-surfaces/pdftovector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -uo pipefail
-IFS=$'\n'
-
-mapfile -t files < <(find . -mindepth 1 -maxdepth 1 -type f -name "*pdf")
-for i in "${files[@]}"; do
-    pdf2svg "$i" "${i%.*}.svg"
-done
